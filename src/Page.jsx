31d1dba4ef91912a575956c7d6e9dee2c68c7773
nukehaus <nuke.twoski@gmail.com>
2026-08-11 18:41:37 -0400

    j

--- src/Page.jsx
+++ src/Page.jsx
@@ -4,7 +4,9 @@ import RN from './RecipeNuke.js'
 class Page extends React.Component {
 
     state = {
-        selectedTags: []
+        text: "",
+        selectedTags: [],
+        recipe: null
     };
 
     constructor(props) {
@@ -15,26 +17,128 @@ class Page extends React.Component {
         console.log("Loaded all data");
     }
 
-    _getClassName(tabName) {
-        return (this.state.currentNav === tabName) 
-            ? "selectednav"
-            : "";
+    _onClickTag(tag) {
+        if (this.state.selectedTags.includes(tag)) {
+            let idx = this.state.selectedTags.indexOf(tag);
+            let tags = this.state.selectedTags;
+            tags.splice(idx, 1);
+            this.setState({selectedTags: tags});
+        }
+        else {
+            let arr = this.state.selectedTags;
+            arr.push(tag);
+            this.setState({selectedTags: arr});
+        }
     }
 
-    _onNavClick(id) {
-        this.setState({currentNav: id});
+    _onClickRecipe(rcp) {
+        this.setState({recipe: rcp});
+    }
+
+    _onRandomize() {
+        if (this.state.selectedTags.length > 0) {
+            let recipes = RN.getRecipes(this.state.selectedTags);
+            let rcp = RN.rand(recipes);
+            this.setState({recipe: rcp});
+        }
+        else {
+            let rcp = RN.rand(RN.database.recipes);
+            this.setState({recipe: rcp});
+        }
+    }
+
+    _onChangeText(str) {
+        let upper = str.toUpperCase();
+        this.setState({text: upper, recipe: RN.getRecipe(upper)});
+    }
+
+    _renderTable() {
+        if (this.state.selectedTags.length > 0) {
+            let recipes = RN.getRecipes(this.state.selectedTags);
+            let tbl = [];
+            for (let [i, recipe] of recipes) {
+                tbl.push(
+                    <div className="tagselected" onClick={(event) => this._onClickRecipe(recipe)}>
+                        {tag}
+                    </div>
+                );
+            }
+            return tbl;
+        }
+        else {
+            return null;
+        }
+    }
+
+    _renderRecipe() {
+        if (this.state.recipe != null) {
+            return (
+                <>
+                    <div>{this.state.recipe.name}</div>
+                    <div>{this.state.recipe.description}</div>
+                </>
+            );
+        }
+        else {
+            return null;
+        }
+    }
+
+    _renderTextBox() {
+        let elements = [];
+        for (let [i, recipe] of RN.database.recipes.entries()) {
+            if (recipe.name.includes(this.state.text)) {
+                elements.push(<option key={"recipe" + i} value={recipe.name}/>);
+            }
+        }
+        return (
+            <div>
+                <datalist id="textbox">
+                    {elements}
+                </datalist>
+                <input className="databaseinput" 
+                    list="textbox"
+                    placeholder="TYPE A RECIPE NAME"
+                    onChange={(event) => this._onChangeText(event.target.value)}/>
+                <button className="" onClick={(event) => this._onRandomize()}>🔄🤪 RANDOMIZE ME</button>
+            </div>
+        )
+    }
+
+    _getClass(tag) {
+        return this.state.selectedTags.includes(tag) ? "tagselected" : "tag";
+    }
+
+    _renderTags() {
+        let tags = [];
+        for (let [i, tag] of RN.database.tags.entries()) {
+            tags.push(
+                <div className={this._getClass(tag)} onClick={(event) => this._onClickTag(tag)}>
+                    {tag}
+                </div>
+            );
+        }
+        return tags;
     }
 
     render() {
-        const header = (
-            <div>
-                hello
-            </div>
-        );
-
         return (
             <div>
-                {header}
+                <div className="center">
+                    CLICK SOME TAGS OR TYPE IN A RECIPE NAME OR SLAM THE RANDOMIZE BUTTON
+                </div>
+                <div className="center">
+                    {this._renderTextBox()}
+                </div>
+                <div className="center">
+                    {this._renderTags()}
+                </div>
+                <div className="center">
+                    {this._renderTable()}
+                </div>
+                <div className="center">
+                    {this._renderRecipe()}
+                </div>
             </div>
         );
     }
--- src/RecipeNuke.js
+++ src/RecipeNuke.js
@@ -34,6 +34,21 @@ RN.rand = function(items) {
     return items[items.length * Math.random() | 0];
 }
 
+RN.getRecipe = function(name) {
+    return RN.database.recipes.find(x => x.name == name);
+}
+
+RN.getRecipes = function(tags) {
+    let recipes = [];
+    for (let [i, rcp] of RN.database.recipes.entries()) {
+        let valid = tags.every(tag => rcp.tags.includes(tag));
+        if (valid) {
+            recipes.push(rcp);
+        }
+    }
+    return recipes;
+}
+
 // The most important function here
 RN.load = function() {
 
@@ -43,12 +58,32 @@ RN.load = function() {
     const loadedRecipes = import.meta.glob('./recipes/*.json', {eager: true});
 
     for (const path in loadedRecipes) {
-        console.log(`Loaded file: ${path}`);
-        loadedRecipes[path]().then((obj) => {
-            console.log(path, obj);
-            let parsed = JSON.parse(content);
-            console.log(path, parsed);
-        });
+        
+        let raw = atob(loadedRecipes[path].default.substring(29));
+        let parsed = JSON.parse(raw);
+
+        parsed.name = parsed.name.toUpperCase();
+        parsed.description = parsed.description.toUpperCase();
+
+        for (let [i, ing] of parsed.ingredients.entries()) {
+            parsed.ingredients[i].name = ing.name.toUpperCase();
+            parsed.ingredients[i].quantity = ing.quantity.toUpperCase();
+        }
+
+        for (let [i, s] of parsed.steps.entries()) {
+            parsed.steps[i] = s.toUpperCase();
+        }
+
+        for (let [i, tag] of parsed.tags.entries()) {
+            parsed.tags[i] = tag.toUpperCase();
+            if (!RN.database.tags.includes(tag.toUpperCase())) {
+                RN.database.tags.push(tag.toUpperCase());
+            }
+        }
+
+        RN.database.recipes.push(parsed);
+
+        console.log(`Loaded recipe at path: ${path}`);
     }
 }
 
